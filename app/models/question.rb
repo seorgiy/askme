b017ba7d55d1e7e4be65a77da54cef24a410b3c3
Seorgiy <george.sergeev2014@gmail.com>
2020-06-01 22:11:04 +0300

    models fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,31 +1,6 @@
-class Question < ActiveRecord::Base
+class Question < ApplicationRecord
   belongs_to :user
 
-  validates :user, :text, presence: true
+  validates :text, presence: true
   validates :text, length: { maximum: 255 }
-
-  before_validation :before_validation
-  after_validation :after_validation
-
-  before_save :before_save
-  after_save :after_save
-
-  before_create :before_create
-  after_create :after_create
-
-  before_update :before_update
-  after_update :after_update
-
-  before_destroy :before_destroy
-  after_destroy :after_destroy
-
-  private
-
-  %w(validation save create update destroy).each do |action|
-    %w(before after).each do |time|
-      define_method("#{time}_#{action}") do
-        puts "******> #{time} #{action}"
-      end
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,23 @@
 require 'openssl'
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   ITERATIONS = 20_000
+  REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  REGEX_USERNAME = /\A[a-z0-9_]+\z/i.freeze
   DIGEST = OpenSSL::Digest::SHA256.new
 
   attr_accessor :password
 
   has_many :questions
 
-  validates :email, :username, presence: true
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  validates :email, :username, uniqueness: true
-  validates :username, length: { maximum: 40 }, format: { with: /\A[a-zA-Z0-9_]+\z/ }
+  validates :email, :username, presence: true, uniqueness: true
+  validates :email, format: { with: REGEX_EMAIL }
+  validates :username, format: { with: REGEX_USERNAME }, length: { maximum: 40 }
   validates :password, presence: true, on: :create
+  validates :password, confirmation: true
 
-  validates_confirmation_of :password
   before_save { username.downcase! }
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -47,5 +37,18 @@ class User < ActiveRecord::Base
     return user if user.password_hash == hashed_password
 
     nil
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
   end
 end

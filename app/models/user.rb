f61b839d61e3838d1a15f652e6f70635a1e4a47f
Seorgiy <george.sergeev2014@gmail.com>
2020-06-01 20:38:02 +0300

    migrate to rails 6

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+require 'openssl'
+
+class User < ActiveRecord::Base
+  ITERATIONS = 20_000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  attr_accessor :password
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :email, :username, uniqueness: true
+  validates :username, length: { maximum: 40 }, format: { with: /\A[a-zA-Z0-9_]+\z/ }
+  validates :password, presence: true, on: :create
+
+  validates_confirmation_of :password
+  before_save { username.downcase! }
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    return nil unless user.present?
+
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+
+    return user if user.password_hash == hashed_password
+
+    nil
+  end
+end
